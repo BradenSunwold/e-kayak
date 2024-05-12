751e27d56f8d27d3096982f838a0d73c24b0db3c
Braden Sunwold <sunwoldb@gmail.com>
2024-05-12 08:30:02 -0700

    Successfully tested Rx and logging of all IMU data. Need to validate speed and mode once have Tx and Rx both running

--- pythonPi/BoatManager.py
+++ pythonPi/BoatManager.py
@@ -6,6 +6,7 @@ import struct
 from pyrf24 import RF24, RF24_PA_LOW
 import schedule
 import yaml
+import logging
 
 from yaml import load, SafeLoader
 
@@ -19,12 +20,29 @@ class StatusType(IntEnum):
   eFaultCleared = 6
 
 class RfManager:
-  def __init__(self, configDictionary):
+  def __init__(self, configDictionary, logger):
     
+    self.mLogger = logger
+    self.mLogger.info('RF Manager Starting Up')
+
     # Initialize class member variables
+    self.mRfReceiveDataFormatMsgOne = 'B?Bffffff'
+    self.mRfReceiveDataFormatMsgTwo = 'Bffffff'
     self.mCurrentBatteryVolt = 100	# Variables to hold last RF messages received
     self.mCurrentMotorMode = 0
     self.mCurrentMotorSpeed = 0
+
+    # IMU member vars
+    self.mCurrentMsgNum = 0
+    self.mCurrentRoll = 0
+    self.mCurrentPitch = 0
+    self.mCurrentYaw = 0
+    self.mCurrentAccelX = 0
+    self.mCurrentAccelY = 0
+    self.mCurrentAccelZ = 0
+    self.mCurrentGyroX = 0
+    self.mCurrentGyroY = 0
+    self.mCurrentGyroZ = 0
 
     # Parse configs
     self.mConfigurator = configDictionary
@@ -32,7 +50,6 @@ class RfManager:
     print(self.mReadDataRate)
     self.mWriteDataRate = self.mConfigurator['rfWriteDataRateInMilliseconds']
     print(self.mWriteDataRate)
-
     
     # Set up radio for coms
     self.mRadio = RF24(22, 0)
@@ -56,23 +73,38 @@ class RfManager:
     time.sleep(.5)
 
   def RfReceive(self):
-    self.mRadio.payload_size = struct.calcsize('?Hfffffffff') # Receive consists of: mode (bool), speed (uint8_t), IMU (F, F, F, F, F, F, F, F, F)
+    self.mRadio.payload_size = 28   # standard incoming data will be 28 bytes
     self.mRadio.listen = True 
-#    print(struct.calcsize('?Hfffffffff'))
     
     has_payload, pipe_number = self.mRadio.available_pipe()
     if has_payload:
       length = self.mRadio.payload_size  # grab the payload length
+      #print(length)
       # fetch 1 payload from RX FIFO
       received = self.mRadio.read(length)  # also clears radio.irq_dr status flag
-      # expecting a little endian float, thus the format string "<f"
-      # received[:4] truncates padded 0s in case dynamic payloads are disabled
-      #payload = struct.unpack("<f", received[:4])[0]
-      #print(struct.unpack('?Hfffffffff', received))
-      #print(struct.calcsize(received))
-      # print details about the received packet
-      #print(f"Received {length} bytes on pipe {pipe_number}: {payload}")
-
+      
+      # If this is first half of message
+      if(struct.unpack("B", received[:1])[0] == 1) :
+          self.mCurrentMsgNum, self.mCurrentMotorMode, self.mCurrentMotorSpeed, self.mCurrentRoll, self.mCurrentPitch, self.mCurrentYaw, self.mCurrentAccelX, self.mCurrentGyroX, self.mCurrentAccelY = struct.unpack(self.mRfReceiveDataFormatMsgOne, received)
+          
+          # Log IMU data
+          self.mLogger.debug('Msg 1')
+          self.mLogger.info('Mode: %s', self.mCurrentMotorMode)
+          self.mLogger.info('Speed: %s', self.mCurrentMotorSpeed)
+          self.mLogger.info('Roll: %.4f', self.mCurrentRoll)
+          self.mLogger.info('Pitch: %.4f', self.mCurrentPitch)
+          self.mLogger.info('Yaw: %.4f', self.mCurrentYaw)
+      else :
+          self.mCurrentMsgNum, self.mCurrentAccelX, self.mCurrentGyroX, self.mCurrentAccelY, self.mCurrentGyroY, self.mCurrentAccelZ, self.mCurrentGyroZ = struct.unpack(self.mRfReceiveDataFormatMsgTwo, received)
+          
+          # Log IMU data
+          self.mLogger.debug('Msg 2')
+          self.mLogger.info('X Acceleration: %.4f', self.mCurrentAccelX)
+          self.mLogger.info('X Gyroscope: %.4f', self.mCurrentGyroX)
+          self.mLogger.info('Y Acceleration: %.4f', self.mCurrentAccelY)
+          self.mLogger.info('Y Gyroscope: %.4f', self.mCurrentGyroY)
+          self.mLogger.info('Z Acceleration: %.4f', self.mCurrentAccelZ)
+          self.mLogger.info('Z Gyroscope: %.4f', self.mCurrentGyroZ)
 
 #def testSend(count: int = 5):
 #    radio.listen = False  # ensures the nRF24L01 is in TX mode
@@ -114,10 +146,25 @@ class RfManager:
 configStream = open("config/config.yaml", 'r')
 config = yaml.safe_load(configStream)
 
-rfManager = RfManager(config['rfManager'])
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='w')
+loggerRf = logging.getLogger('loggerRf')
 
-#while(1) :
-#  rfManager.RfReceive()
+# Create logging handlers
+rfFileHandler = logging.FileHandler(config['rfManager']['rfLoggerFilePath'])
+rfFileHandler.setLevel(logging.DEBUG)
+
+rfLoggerFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+rfFileHandler.setFormatter(rfLoggerFormatter)
+
+loggerRf.addHandler(rfFileHandler)
+
+
+# Create rfManager object
+rfManager = RfManager(config['rfManager'], loggerRf)
+
+while(1) :
+  rfManager.RfReceive()
 
 batteryVoltage = 25.0
 
